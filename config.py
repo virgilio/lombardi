c161e3d3d45a40875430959586a6e5f89a27c255
Virgilio Santos <virgilio.santos@gmail.com>
2016-09-28 16:59:24 -0300

    slack: small slack middleware example config: add SLACK_USER option

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ MODULES = [
 ]
 
 SLACK_TOKEN = ""
+SLACK_USER = ""
 
 MODULES = (".".join([MODULE_ROOT, m]) for m in MODULES)
 MIDDLEWARE = ".".join([MIDDLEWARE_ROOT, "slack"])
--- middlewares/slack.py
+++ middlewares/slack.py
@@ -1,8 +1,37 @@
-def process_message(message):
-    obj = type('obj', (object,), {})
-    obj.message = message
-    obj.options = None
-    return obj()
+from config import SLACK_USER as user
 
-def send_message(message, options):
-    pass
+class Middleware():
+
+    def __init__(self, sc):
+        self.sc = sc
+
+    def process_message(self, message):
+        # if message.has_key("user") and message["user"] == "U2H6221EG":
+        if message.has_key("user") and message["user"] == user:
+            return None
+
+        obj = type('obj', (object,), {})
+
+        if message.has_key("text"):
+            obj.message = message["text"]
+        else:
+            obj.message = message
+
+        result = obj()
+
+        options = type('options', (object,), {})
+        if message.has_key("channel"):
+            print message["channel"]
+            options.channel = message["channel"]
+        else:
+            options.channel = "#polanco"
+
+        result.options = options
+
+        return result
+
+    def send_message(self, message, options):
+        self.sc.api_call("chat.postMessage", as_user="true:", channel=options.channel, text=message)
+
+    def send_file(self, message, filename, options):
+        pass
